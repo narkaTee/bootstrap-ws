62a2425f67972d6d39681aa4caf249c36da4e29f
narkaTee <narkaTee@users.noreply.github.com>
2025-12-17 15:34:04 +0100

    Create a sandbox container
    The base for a dev sandbox container that sandboxes installation of
    dependencies and other potential dangerous/system cluttering stuff
    into a container.

---
+++ sandbox/pre-warm-tools.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+# The wonderful line makes sure p10k can initilize and download gitstatusd
+echo exit | script -qec zsh /dev/null >/dev/null
+
+. /home/dev/.config/setup/tools.sh
+# install n
+yes | n
+# the path update and unset in the n bootstrap function does not work when running a script
+# in an interactive shell it works flawless...
+# For now this is good enough to source the tools setup again and unset the n function manually
+. /home/dev/.config/setup/tools.sh
+unset -f n
+# pre install nodejs lts
+n install lts
+
+# pre install sdk man, yep that is quite sketchy but manually installing is complicated
+curl -s "https://get.sdkman.io?ci=true&rcupdate=false" | bash
