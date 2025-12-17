34063a6309aae26492bd55190ec973eee03637f8
narkaTee <narkaTee@users.noreply.github.com>
2025-12-17 15:34:04 +0100

    Create a sandbox container
    The base for a dev sandbox container that sandboxes installation of
    dependencies and other potential dangerous/system cluttering stuff
    into a container.

---
+++ sandbox/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Inject SSH keys from environment variable if provided
+if [ -n "$SANDBOX_SSH_KEYS" ]; then
+    echo "$SANDBOX_SSH_KEYS" | base64 -d > /home/dev/.ssh/authorized_keys
+    chmod 600 /home/dev/.ssh/authorized_keys
+    chown -R dev:dev /home/dev/.ssh
+fi
+
+if [ $# -gt 0 ]; then
+    exec "$@"
+fi
+
+exec /usr/sbin/sshd -D -e
